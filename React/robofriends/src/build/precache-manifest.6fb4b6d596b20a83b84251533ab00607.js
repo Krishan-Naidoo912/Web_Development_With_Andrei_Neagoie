ba22029a34baebb2ac56d963bea6c844a3ae2359
Krishan-Naidoo <56049699+Krishan-Naidoo@users.noreply.github.com>
2020-02-07 23:56:27 +0200

    Updating RoboFriends ReactApp ScrollBox build

---
+++ React/robofriends/src/build/precache-manifest.6fb4b6d596b20a83b84251533ab00607.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dd4c83c9cde665676f1c04ec643c50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22dcd0596ff61b550033",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fba2eb93ae6f7a5570ff",
+    "url": "/static/css/main.7f3eb88f.chunk.css"
+  },
+  {
+    "revision": "22dcd0596ff61b550033",
+    "url": "/static/js/2.29dae0ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.29dae0ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fba2eb93ae6f7a5570ff",
+    "url": "/static/js/main.430dba19.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  }
+]);
